218ac621cc73e53204eeb51e63b748ef1765b2d4
Girish <gspande7@gmail.com>
2023-10-19 09:26:37 -0400

    cleaned up js file

--- main.js
+++ main.js
@@ -13,12 +13,7 @@ buttons.map( button => {
                   display.innerText = "Error"
               }
               break;
-          case '←':
-              if (display.innerText){
-                 display.innerText = display.innerText.slice(0, -1);
-              }
-              break;
-          default:
+           default:
               display.innerText += e.target.innerText;
       }
   });
